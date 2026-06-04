01e8f532ea98c036feee1bf524f6c4a131be6a37
juanchila1000-del <juanchila1000@gmail.com>
2026-06-04 15:53:04 -0300

    Update createSessionUtil.ts

--- src/util/createSessionUtil.ts
+++ src/util/createSessionUtil.ts
@@ -260,9 +260,7 @@ export default class CreateSessionUtil {
     await this.checkStateSession(client, req);
     await this.listenMessages(client, req);
 
-    setInterval(() => {
-    req.logger.info(`KEEPALIVE ${client.session}`);
-    }, 30000);
+   
     
     if (req.serverOptions.webhook.listenAcks) {
       await this.listenAcks(client, req);
